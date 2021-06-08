20f7937e485c65f5e71f5e465e851716f966d420
ShinyWobbuffet <63820638+ShinyWobbuffet@users.noreply.github.com>
2021-06-08 16:34:48 -0400

    Create tracklist.js

---
+++ commands/tracklist.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const config = require("./config")
+
+module.exports = (message, tracklist) => {
+  args = message.content.split(" ");
+  if (args.length < 2) {
+    message.channel.send(`Current tracklist: ${tracklist}`);
+  } else if (args.length > 2) {
+    message.channel.send("Too many parameters!");
+  } else {
+    if (!fs.existsSync("./tracks/" + tracklist + ".txt")) {
+      message.channel.send("The tracklist must exist. Supported lists: pokemon.")
+    } else {
+      let newTracklist = args[1];
+      config.tracks.custom[message.guild.id.toString()] = newTracklist;
+      fs.writeFile("config.json", JSON.stringify(config, null, 2), () => {
+        if (err) {
+          message.channel.send("An error occurred.");
+        } else {
+          message.channel.send("Tracklist changed.")
+        }
+      })
+    }
+  }
+};
