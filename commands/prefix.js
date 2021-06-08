131f7032e0fbd241f623cd42418c64abe6ead748
ShinyWobbuffet <63820638+ShinyWobbuffet@users.noreply.github.com>
2021-06-08 16:07:29 -0400

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const config = require("../config")
 
 module.exports = (message, prefix) => {
   args = message.content.split(" ");
@@ -7,7 +8,14 @@ module.exports = (message, prefix) => {
   } else if (args.length > 2) {
     message.channel.send("Too many parameters!");
   } else {
-    let prefix = args[1];
-    message.channel.send("Prefix changed.");
+    let newPrefix = args[1];
+    config.prefix.custom[message.guild.id] = newPrefix;
+    fs.writeFile("config.json", JSON.stringify(config, null, 2), () => {
+      if (err) {
+        message.channel.send("An error occurred.");
+      } else {
+        message.channel.send("Prefix changed.")
+      }
+    })
   }
 };
