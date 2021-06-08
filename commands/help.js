c2c745d3679334e28860291d5d28e108aa4c5627
ShinyWobbuffet <63820638+ShinyWobbuffet@users.noreply.github.com>
2021-06-08 19:52:44 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,10 +3,12 @@ module.exports = (message, prefix) => {
 SongBirb, made by <@!544169448890826757>, <@!495942812542697472> and <@!538557982955536410>
 Current prefix: ${prefix}
 \`\`\`
-${prefix}help   -- Display this message.
-${prefix}leave  -- Leave the voice chat.
-${prefix}summon -- Summon SongBirb to your VC.
-${prefix}ping   -- Check if the bot is up.
-${prefix}prefix -- Change or check the prefix of the server.\
+${prefix}help      -- Display this message.
+${prefix}leave     -- Leave the voice chat.
+${prefix}summon    -- Summon SongBirb to your VC.
+${prefix}ping      -- Check if the bot is up.
+${prefix}prefix    -- Change or check the prefix of the server.\
+${prefix}volume    -- Change the volume of the bot.
+#{prefix}tracklist -- Change the tracklist the bot uses.
   \`\`\``);
 };
