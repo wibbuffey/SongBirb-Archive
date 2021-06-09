f3ebd446617de28bcd207815721587e5fc05c46c
ShinyWobbuffet <63820638+ShinyWobbuffet@users.noreply.github.com>
2021-06-09 13:56:59 -0400

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -1,6 +1,7 @@
 module.exports = (message) => {
   try {
     message.member.voice.channel.leave();
+    message.channel.send("I left the VC.")
   } catch (e) {
     message.channel.send("You're not in a VC with me.");
   }
