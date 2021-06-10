675351e1b6e5a8411e5ed554866ae85fe44371c9
ShinyWobbuffet <63820638+ShinyWobbuffet@users.noreply.github.com>
2021-06-10 09:21:37 -0400

    Update fav.js

--- commands/fav.js
+++ commands/fav.js
@@ -15,17 +15,17 @@ Array.prototype.remove = function () {
   return this;
 };
 
-module.exports = (message) => {
-  if (config.usrfav[global.song]) {
-    if (!config.usrfav[global.song].includes(message.author.id)) {
-      config.usrfav[global.song].push(message.author.id);
+module.exports = (message, song) => {
+  if (config.usrfav[song]) {
+    if (!config.usrfav[song].includes(message.author.id)) {
+      config.usrfav[song].push(message.author.id);
       message.channel.send("Successfully favorited.");
     } else {
-      config.usrfav[global.song].remove(message.author.id);
+      config.usrfav[song].remove(message.author.id);
       message.channel.send("Successfully unfavorited.");
     }
   } else {
-    config.usrfav[global.song] = [message.author.id];
+    config.usrfav[song] = [message.author.id];
     message.channel.send("Successfully favorited.");
   }
   fs.writeFileSync("./config.json", JSON.stringify(config, null, 2));
