b7c30467040946c46ce021633e45a1919625b9a0
ShinyWobbuffet <63820638+ShinyWobbuffet@users.noreply.github.com>
2021-08-12 21:46:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
 const config = require("./config");
-
-const ping = require("./commands/ping");
-const summon = require("./commands/summon");
-const leave = require("./commands/leave");
-const prefix = require("./commands/prefix");
-const help = require("./commands/help");
-const tracklist = require("./commands/tracklist");
-const volume = require("./commands/volume");
-const fav = require("./commands/fav");
-
-const check = (prefix) => {
-  if (!prefix) {
-    return config.prefix.default;
-  } else {
-    return prefix;
-  }
-};
-
-const getVolume = (message) => {
-  customVolume = config.volume.custom[message.guild.id.toString()];
-  if (customVolume) {
-    return customVolume;
-  } else {
-    return config.volume.default;
-  }
-};
-
-const getTracklist = (message) => {
-  customTracklist = config.tracks.custom[message.guild.id.toString()];
-  if (customTracklist) {
-    return customTracklist;
-  } else {
-    return config.tracks.default;
-  }
-};
+const commands = require("./commands");
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}.`);
@@ -44,28 +11,13 @@ client.on("ready", () => {
 
 try {
   client.on("message", (message) => {
-    const currentPrefix = check(config.prefix.custom[message.guild.id]);
-    const currentVolume = getVolume(message);
-    const currentTracklist = getTracklist(message);
-
-    if (message.content.startsWith(currentPrefix + "ping")) {
-      ping(message);
-    } else if (message.content.startsWith(currentPrefix + "summon")) {
-      summon(message, client, currentVolume, currentTracklist);
-    } else if (message.content.startsWith(currentPrefix + "leave")) {
-      leave(message);
-    } else if (message.content.startsWith(currentPrefix + "help")) {
-      help(message, currentPrefix);
-    } else if (message.content.startsWith(currentPrefix + "prefix")) {
-      prefix(message, currentPrefix);
-    } else if (message.content.startsWith(currentPrefix + "volume")) {
-      volume(message, currentVolume);
-    } else if (message.content.startsWith(currentPrefix + "tracklist")) {
-      tracklist(message, currentTracklist);
-    } else if (message.content.startsWith(currentPrefix + "fav")) {
-      fav(message)
-    } else if (message.content.startsWith(currentPrefix)) {
-      message.channel.send("Command not found. :(");
+    const prefix = config.prefix.custom[message.guild.id] || config.prefix.default;
+    const command = message.split(prefix.length - 1, message.length - 1)
+    
+    if (message.startsWith("prefix") && commands[command]) {
+      commands[command]()
+    } else {
+      message.channel.send("Command not found!")
     }
   });
 } catch {
